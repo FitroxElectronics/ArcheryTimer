c8d92cee5c366ea5ee954388e6f9d9118230df05
Fitrox Electronics <33017282+FitroxElectronics@users.noreply.github.com>
2021-02-02 02:58:44 +0700

    Update SETTING.ino

--- SETTING.ino
+++ SETTING.ino
@@ -6,60 +6,54 @@ void SET_PROG(){
     digitalWrite(RED_LIGHT, LOW);
     digitalWrite(YELLOW_LIGHT, HIGH);
     digitalWrite(GREEN_LIGHT, LOW);
-    LCD.clear();
-    LCD.setCursor(0, 0);
-    LCD.print("Setting");
-    LCD.setCursor(0, 1);
-    LCD.print("CALL Time ");
+    MAIN_DISPLAY.setChar(0, 4, 'c', false);
     SET_CALLTIME();
   }
 
   // Stage = 1 set shooting time
   while(SETTING_STAGE == 1){
-    LCD.clear();
-  LCD.setCursor(0, 0);
-  LCD.print("Setting");
-  LCD.setCursor(0, 1);
-  LCD.print("SHOOTING Time ");
+  MAIN_DISPLAY.setChar(0, 4, 'l', false);
   SET_SHOOTINGTIME();
   }
   
   digitalWrite(DEVICE_INDICATOR, LOW);
-  LCD.clear();
 }
 
 
 /* -------------------------------------------- Call Time Setting -------------------------------------------- */
 void SET_CALLTIME(){
+  int DIGIT_2 = CALL_TIME/100;
+  int DIGIT_1 = (CALL_TIME%100)/10;
+  int DIGIT_0 = (CALL_TIME%100)%10;
+  
   // Set call time 0 - 100
   if(digitalRead(UP_BUTTON) == HIGH){
-    delay(50);
+    delay(10);
     CALL_TIME = CALL_TIME + 1;
     if(CALL_TIME > 100){
       CALL_TIME = 0;
     }
+  // Calculate digit to show on Display
+  DIGIT_2 = CALL_TIME/100;
+  DIGIT_1 = (CALL_TIME%100)/10;
+  DIGIT_0 = (CALL_TIME%100)%10;
   }
   if(digitalRead(DOWN_BUTTON) == HIGH){
-    delay(50);
+    delay(10);
     CALL_TIME = CALL_TIME - 1;
     if(CALL_TIME < 0){
       CALL_TIME = 100;
     }
-  }
-
   // Calculate digit to show on Display
-  int DIGIT_2 = CALL_TIME/100;
-  int DIGIT_1 = (CALL_TIME%100)/10;
-  int DIGIT_0 = (CALL_TIME%100)%10;
-
-  // Show on LCD
-  LCD.setCursor(15, 1);
-  LCD.print(CALL_TIME);
+  DIGIT_2 = CALL_TIME/100;
+  DIGIT_1 = (CALL_TIME%100)/10;
+  DIGIT_0 = (CALL_TIME%100)%10;
+  }
 
   // Show on display
     MAIN_DISPLAY.setDigit(0, 0, DIGIT_0, false);
     MAIN_DISPLAY.setDigit(0, 1, DIGIT_1, false);
-    MAIN_DISPLAY.setDigit(0, 2, DIGIT_2, false);
+    MAIN_DISPLAY.setDigit(0, 3, DIGIT_2, false);
 
   // if press START, memory and go to next stage
   if(digitalRead(START_BUTTON) == HIGH){
@@ -73,35 +67,38 @@ void SET_CALLTIME(){
 
 /* ------------------------------------------ Shooting Time Setting ------------------------------------------ */
 void SET_SHOOTINGTIME(){
+  int DIGIT_2 = SHOOTING_TIME/100;
+  int DIGIT_1 = (SHOOTING_TIME%100)/10;
+  int DIGIT_0 = (SHOOTING_TIME%100)%10;
+  
   // Set shooting time 0 - 600
   if(digitalRead(UP_BUTTON) == HIGH){
-    delay(50);
+    delay(10);
     SHOOTING_TIME =SHOOTING_TIME + 1;
     if(SHOOTING_TIME > 600){
       SHOOTING_TIME = 0;
     }
+    // Calculate digit to show on Display
+  DIGIT_2 = SHOOTING_TIME/100;
+  DIGIT_1 = (SHOOTING_TIME%100)/10;
+  DIGIT_0 = (SHOOTING_TIME%100)%10;
   }
   if(digitalRead(DOWN_BUTTON) == HIGH){
-    delay(50);
+    delay(10);
     SHOOTING_TIME = SHOOTING_TIME - 1;
     if(SHOOTING_TIME < 0){
       SHOOTING_TIME = 600;
     }
+    // Calculate digit to show on Display
+  DIGIT_2 = SHOOTING_TIME/100;
+  DIGIT_1 = (SHOOTING_TIME%100)/10;
+  DIGIT_0 = (SHOOTING_TIME%100)%10;
   }
-
-  // Calculate digit to show on Display
-  int DIGIT_2 = SHOOTING_TIME/100;
-  int DIGIT_1 = (SHOOTING_TIME%100)/10;
-  int DIGIT_0 = (SHOOTING_TIME%100)%10;
-
-  // Show on LCD
-  LCD.setCursor(15, 1);
-  LCD.print(SHOOTING_TIME);
 
   // Show on display
     MAIN_DISPLAY.setDigit(0, 0, DIGIT_0, false);
     MAIN_DISPLAY.setDigit(0, 1, DIGIT_1, false);
-    MAIN_DISPLAY.setDigit(0, 2, DIGIT_2, false);
+    MAIN_DISPLAY.setDigit(0, 3, DIGIT_2, false);
 
   // if press START, memory and go to next stage
   if(digitalRead(START_BUTTON) == HIGH){
